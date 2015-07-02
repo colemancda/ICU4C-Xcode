9b2d83fe28c34d4cd1caac284a5d8eb8987bab32
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-07-01 23:34:58 -0500

    created project and added sources

---
+++ ICU/ICU/ICU.h
@@ -0,0 +1,21 @@
+//
+//  ICU.h
+//  ICU
+//
+//  Created by Alsey Coleman Miller on 7/1/15.
+//  Copyright © 2015 International Components for Unicode Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ICU.
+FOUNDATION_EXPORT double ICUVersionNumber;
+
+//! Project version string for ICU.
+FOUNDATION_EXPORT const unsigned char ICUVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICU/PublicHeader.h>
+
+
+
+
